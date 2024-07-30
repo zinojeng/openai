69d1f0c08b4cd337f19e60c0159b5503b828fa63
zino <yaohsientseng@YAOHSIENdeMacBook-Air.local>
2024-07-30 08:02:28 +0800

    Update hide streamlit 14

--- paired_app.py
+++ paired_app.py
@@ -44,12 +44,6 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 
 # Sidebar for API key input
