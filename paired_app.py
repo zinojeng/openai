a8fab8fbb528e224d419cc9580149257169ad9c8
zino <yaohsientseng@YAOHSIENdeMacBook-Air.local>
2024-07-30 21:07:32 +0800

    Update hide streamlit 18

--- paired_app.py
+++ paired_app.py
@@ -22,11 +22,10 @@ nltk.download('punkt', quiet=True)
 st.set_page_config(page_title="Translation Agent", layout="wide")
 
 # Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#MainMenu {
-  visibility: hidden;
-}
-"""
+hide_github_icon = “”"
+
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+“”"
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 # Sidebar for API key input
